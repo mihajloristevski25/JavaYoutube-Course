5c79f275bd2af63db2b62cc3877c6ba474f0fd2c
Mihajlo Ristevski <80358616+mihajloristevski25@users.noreply.github.com>
2021-11-01 17:21:46 +0100

    Add files via upload

---
+++ multipleClass/multipleClass.java
@@ -0,0 +1,9 @@
+package multipleClass;
+
+
+public class multipleClass {
+	public static void main(String[] args) {
+		multipleClass1 obj = new multipleClass1();
+		obj.simplemessage();
+	}
+}
---
+++ multipleClass/multipleClass1.java
@@ -0,0 +1,8 @@
+package multipleClass;
+
+
+public class multipleClass1 {
+	public void simplemessage() {
+		System.out.println("This is class multipleClass1");
+	}
+}
