d4151806cb3113f685a0f53c6b9db14fbeb473c2
Mihajlo Ristevski <80358616+mihajloristevski25@users.noreply.github.com>
2021-11-12 19:38:29 +0100

    Add files via upload

---
+++ radioButton/glavna.java
@@ -0,0 +1,12 @@
+package radioButton;
+import javax.swing.JFrame;
+
+
+public class glavna {
+	public static void main(String[] args) {
+		radiobutton rb = new radiobutton();
+		rb.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		rb.setSize(200,200);
+		rb.setVisible(true);
+	}
+}
