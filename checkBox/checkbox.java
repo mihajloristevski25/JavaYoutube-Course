a26a1a96d6c3477728c486a519892b4fe4dc653c
Mihajlo Ristevski <80358616+mihajloristevski25@users.noreply.github.com>
2021-11-12 19:42:14 +0100

    Add files via upload

---
+++ checkBox/checkbox.java
@@ -0,0 +1,47 @@
+package checkBox;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+public class checkbox extends JFrame {
+	
+	private JTextField tf;
+	private JCheckBox boldbox;
+	private JCheckBox italicbox;
+	
+	public checkbox() {
+		super("Title");
+		setLayout(new FlowLayout());
+		tf = new JTextField("This is a sentece you can change",20);
+		tf.setFont(new Font("Serif",Font.PLAIN,14));
+		add(tf);
+		
+		boldbox = new JCheckBox("Bold");
+		italicbox = new JCheckBox("Italic");
+		add(boldbox);
+		add(italicbox);
+	
+		HandlerClass handler = new HandlerClass();
+		boldbox.addItemListener(handler);
+		italicbox.addItemListener(handler);
+		
+	}
+	private class HandlerClass implements ItemListener{
+		public void itemStateChanged(ItemEvent event) {
+				Font font = null;
+				
+				if(boldbox.isSelected() && italicbox.isSelected()) {
+					font = new Font("Serif", Font.BOLD + Font.ITALIC , 14);
+				}else if (boldbox.isSelected()) {
+					font = new Font("Serif", Font.BOLD,14);
+				}else if (italicbox.isSelected()) {
+					font = new Font("Serif",Font.ITALIC,14);
+				}else
+					font = new Font("Serif",Font.PLAIN,14);
+				
+				tf.setFont(font);
+		}
+	}
+	
+}
---
+++ checkBox/glavna.java
@@ -0,0 +1,11 @@
+package checkBox;
+import javax.swing.JFrame;
+public class glavna {
+	public static void main(String[] args) {
+		checkbox go = new checkbox();
+		go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		go.setSize(300,200);
+		go.setVisible(true);
+		
+	}
+}
